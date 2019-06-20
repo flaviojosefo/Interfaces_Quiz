196f3f2b090acb75a9d6dd1f70303c3342734e49
fs000 <flaviosantos002010@hotmail.com>
2019-06-20 20:50:03 +0100

    Quiz now has a few elements including a working slider

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+namespace MathQuiz {
+
+    public class Quiz : MonoBehaviour {
+
+        [Header("UI Elements")]
+        public GameObject mainMenu;
+        public GameObject quiz;
+        
+        [Header("Slider")]
+        public float originalSliderTime;
+        public Image slider;
+        public TMP_Text sliderTime;
+        private float _currentSliderTime;
+
+        [Header("Buttons")]
+        public Button startButton;
+
+        void Start() {
+
+            SetSliderDefaultValues();
+        }
+
+        void Update() {
+
+            UpdateSlider();
+        }
+
+        private void CheckInput() {
+
+            if (Input.GetKeyDown(KeyCode.DownArrow)) {
+
+                startButton.Select();
+            }
+        }
+
+        private void SetSliderDefaultValues() {
+            
+            _currentSliderTime = originalSliderTime;
+            slider.fillAmount = 0;
+            sliderTime.text = originalSliderTime.ToString();
+        }
+
+        private void UpdateSlider() {
+
+            if (_currentSliderTime > 0 && quiz.activeSelf) {
+
+                _currentSliderTime -= Time.deltaTime;
+                slider.fillAmount += Time.deltaTime / originalSliderTime;
+                sliderTime.text = ((int)_currentSliderTime).ToString();
+
+            } else {
+
+                // END QUIZ
+            }
+        }
+
+        public void EnableMenu() {
+
+            mainMenu.SetActive(true);
+        }
+
+        public void DisableMenu() {
+
+            mainMenu.SetActive(false);
+            StartQuiz();
+        }
+
+        private void StartQuiz() {
+
+            // GetQuestion();
+            // GetAnswers();
+            SetSliderDefaultValues();
+            quiz.SetActive(true);
+        }
+
+        // 'public' to use in button if the person wants to exit
+        public void EndQuiz() {
+
+
+        }
+
+        public void Quit() {
+
+            print("Yes, it quit.");
+            Application.Quit();
+        }
+    }
+}
