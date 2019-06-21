a3f0b7d26745941a2d56550c0e9c01d6f4e9232a
fs000 <flaviosantos002010@hotmail.com>
2019-06-21 14:04:39 +0100

    Finish prototype

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -12,7 +12,8 @@ namespace MathQuiz {
         [Header("UI Elements")]
         public GameObject mainMenu;
         public GameObject quiz;
-        
+        public GameObject endGame;
+
         [Header("Slider")]
         public float originalSliderTime;
         public Image slider;
@@ -21,27 +22,46 @@ namespace MathQuiz {
 
         [Header("Q&A Elements")]
         public TMP_Text question;
+        public Image questionImg;
         public Button answerButton;
         public Transform answersParent;
+
+        [Header("Endscreen Elements")]
+        public Image background;
+        public TMP_Text grade;
+        public TMP_Text report;
 
         [Header("Questions")]
         public List<Questions> questions;
 
+        [Header("Aid Buttons")]
+        public List<GameObject> aidButtons;
+
         // Already asked questions
         private List<int> aaQuestions;
 
+        private List<int> wrongQuestions;
+
+        private int currentQuestion;
+
+        private int totalAnswers;
+        private int totalCorrectAnwers;
+
         void Start() {
+
+            totalAnswers = 0;
+            totalCorrectAnwers = 0;
 
             ResetTimer();
         }
 
         void Update() {
-            
+
             UpdateSlider();
         }
 
         private void ResetTimer() {
-            
+
             _currentSliderTime = originalSliderTime;
             slider.fillAmount = 0;
             sliderTime.text = originalSliderTime.ToString();
@@ -74,8 +94,12 @@ namespace MathQuiz {
 
         private void StartQuiz() {
 
-            GetQuestion();
-            GetAnswers(0);
+            wrongQuestions = new List<int>();
+
+            totalAnswers = 0;
+            totalCorrectAnwers = 0;
+
+            GetAnswers(GetQuestion());
             ResetTimer();
             quiz.SetActive(true);
         }
@@ -83,6 +107,9 @@ namespace MathQuiz {
         // 'public' to use in button if the person wants to exit
         public void EndQuiz() {
 
+            aaQuestions = new List<int>();
+
+            endGame.SetActive(false);
             quiz.SetActive(false);
             mainMenu.SetActive(true);
         }
@@ -107,12 +134,21 @@ namespace MathQuiz {
 
             int n = Random.Range(0, questions.Count);
 
-            if (!aaQuestions.Contains(n)) {
+            int i = 0;
 
-                question.text = questions[n].question;
-                aaQuestions.Add(n);
-                return n;
+            while (aaQuestions.Contains(n) && i < 1000) {
+
+                n = Random.Range(0, questions.Count);
+                i++;
             }
+
+            if (i >= 1000) ShowEndScreen();
+
+            question.text = questions[n].question;
+            questionImg.sprite = questions[n].questionImage;
+            aaQuestions.Add(n);
+
+            currentQuestion = n;
 
             return n;
         }
@@ -120,20 +156,93 @@ namespace MathQuiz {
         private void GetAnswers(int questionID) {
 
             DeleteAllButtons();
-
-            // always 4 buttons?
-            for (int i = 0; i < 4; i++) {
+            
+            for (int i = 0; i < questions[questionID].answers.Count; i++) {
 
                 Button b = Instantiate(answerButton, answersParent);
-                b.onClick.AddListener(delegate { GetAnswers(GetQuestion()); ResetTimer(); });
+                b.transform.GetChild(0).GetComponent<TMP_Text>().text = questions[questionID].answers[i].answer;
+
+                int tempI = i;
+                b.onClick.AddListener(delegate { VerifyAnswer(questionID, tempI); GetAnswers(GetQuestion()); ResetTimer(); });
             }
+        }
+
+        private void VerifyAnswer(int questionID, int answerID) {
+
+            for (int i = 0; i < questions[questionID].answers.Count; i++) {
+
+                if (i == answerID && questions[questionID].answers[answerID].isCorrect) {
+                    
+                    totalAnswers += 1;
+                    totalCorrectAnwers += 1;
+                    return;
+                }
+            }
+
+            wrongQuestions.Add(questionID);
+            totalAnswers += 1;
         }
 
         private void DeleteAllButtons() {
 
             foreach (Transform button in answersParent) {
-                
+
                 Destroy(button.gameObject);
+            }
+        }
+
+        private void ShowEndScreen() {
+
+            ShowIncorrectAnswers();
+
+            if (totalCorrectAnwers <= 2) {
+
+                grade.text = "MAU";
+                background.color = Color.red;
+
+            } else if (totalCorrectAnwers <= 5) {
+
+                grade.text = "BOM";
+                background.color = Color.yellow;
+
+            } else {
+
+                grade.text = "MUITO BOM";
+                background.color = Color.green;
+            }
+
+            endGame.SetActive(true);
+        }
+
+        private void ShowIncorrectAnswers() {
+
+            if (totalCorrectAnwers == questions.Count) {
+
+                report.text = "Acertaste todas as perguntas! Parabéns!";
+                return;
+
+            } else {
+
+                report.text = $"Acertaste {totalCorrectAnwers} em {totalAnswers} perguntas! \n\n";
+
+                foreach (int i in wrongQuestions) {
+
+                    report.text += $"Erraste a pergunta {i+1}. \n";
+                }
+            }
+        }
+
+        private void FiftyFifty() {
+
+            int n = 0;
+
+            for (int i = 0; i < questions[currentQuestion].answers.Count; i++) {
+
+                if (!questions[currentQuestion].answers[i].isCorrect && n < questions[currentQuestion].answers.Count/2) {
+
+                    Destroy(answersParent.GetChild(i).gameObject);
+                    n++;
+                }
             }
         }
 
@@ -143,7 +252,8 @@ namespace MathQuiz {
 
                 case 0:
 
-                    // remove 2 wrong answers
+                    FiftyFifty();
+                    Destroy(aidButtons[0]);
                     break;
 
                 case 1:
